f19093134ae629ec034c90a7816ff82dc24afcc5
Himanshu <163458979+himanshug0503@users.noreply.github.com>
2025-07-23 15:19:47 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/components/Dashboard/Dashboard.module.css
    	modified:   src/components/Groups/CreateGroup.module.css
    	modified:   src/index.css
    	modified:   src/pages/GroupPage.jsx
    	modified:   src/pages/Home.module.css
    	modified:   src/pages/loginPage.css

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -4,15 +4,11 @@ import GroupList from "../components/Groups/GroupList";
 
 export default function GroupPage() {
   return (
-    <div
-      style={{
-        backgroundColor: "#f2f2f2",
-        minHeight: "100vh",
-        padding: "2rem",
-      }}
-    >
-      <CreateGroup />
-      <GroupList />
+    <div className="page">
+      <div className="container">
+        <CreateGroup />
+        <GroupList />
+      </div>
     </div>
   );
 }
